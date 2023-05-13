d5a46f582db0afe1ddd3499f10a4e85b2b511001
Emi <89019222+NalbandyanE@users.noreply.github.com>
2023-05-13 17:01:18 +0400

    Update check.cpp

--- check.cpp
+++ check.cpp
@@ -1,72 +1,3 @@
-// #include <iostream>
-// #include <fstream>
-// #include <sstream>
-// #include <vector>
-// #include "main.h"
-
-// bool sum(int a, int b, int sum) {
-//     return a + b == sum;
-// }
-
-// int main() {
-//     std::string button;
-//     std::cout << "Enter 'RUN' or 'SUBMIT': ";
-//     std::cin >> button;
-//     std::string file_name;
-//     if(button == "RUN"){
-//         file_name = "run_cases.txt";
-//     } 
-//     if(button == "SUBMIT"){
-//         file_name = "submit_cases.txt";
-//     }  
-//     std::ifstream infile(file_name);
-//     std::ofstream outfile("result.txt");
-//     if (!infile) {
-//         std::cerr << "Error opening input file\n";
-//         return 1;
-//     }
-//     if (!outfile) {
-//         std::cerr << "Error opening output file\n";
-//         return 1;
-//     }
-//     std::string line;
-//     std::vector<bool> results;
-//     int line_count = 0;
-//     while (std::getline(infile, line)) {
-//         ++line_count;
-//         std::istringstream ss(line);
-//         int a, b, c;
-//         if (ss >> a >> b >> c) {
-//             int trueSum = c;
-//             int yourSum = solution(a, b);
-//             bool result = (yourSum == trueSum);
-//             results.push_back(result);
-//             if (!result) {
-//                 outfile << "Case " << line_count << " is false\n";
-//                 outfile << "Expected: " << trueSum << "\n";
-//                 outfile << "Output: " << yourSum << "\n";
-//             }
-//         }
-//         else {
-//             std::cerr << "Error parsing line " << line_count << std::endl;
-//             return 1;
-//         }
-//     }
-//     bool all_results = true;
-//     for (bool r : results) {
-//         if (!r) {
-//             all_results = false;
-//             break;
-//         }
-//     }
-//     if (all_results) {
-//         outfile << "Success\n";
-//     }
-
-//     return 0;
-// }
-
-
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -110,7 +41,8 @@ int main() {
                                {"expected", trueSum},
                                {"output", yourSum},
                                {"result", result}});
-        } else {
+        } 
+	else {
             std::cerr << "Error parsing line " << line_count << std::endl;
             return 1;
         }
@@ -118,7 +50,7 @@ int main() {
 
     bool all_results = true;
     for (auto& r : results) {
-		all_results &= r.at("result").get<bool>();
+	all_results &= r.at("result").get<bool>();
         if (!r["result"]) {
             std::cout << "Case " << r["case"] << " is false\n";
             std::cout << "Expected: " << r["expected"] << "\n";
